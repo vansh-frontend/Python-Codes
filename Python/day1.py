d74bfa182ab49284c94cc7181523736503c65001
vansh-frontend <vanshdhalor04@gmail.com>
2024-12-04 22:30:22 +0530

    day1 update

--- Python/day1.py
+++ Python/day1.py
@@ -1,1 +1,9 @@
-print("hello world")
+def name():
+    while True:
+        a = input("enter your name: ").strip()
+        if a.isalpha():
+            return f"hello {a} nice to meet you."
+        else:
+            print("invalid please enter (letters only)")
+            
+print(name())
